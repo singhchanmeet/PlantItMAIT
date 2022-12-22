8bdbf085bb39d77176e57929490d3ae57c8e00f0
Chanmeet <chanmeetsinghsahni@gmail.com>
2022-12-22 16:44:40 +0530

    4th Commit

--- home/models.py
+++ home/models.py
@@ -10,13 +10,19 @@ class Contact(models.Model):
         return self.name
     
 class Plant(models.Model) :
-    plant_id=models.CharField(max_length=2,default='')
-    plant_name=models.CharField(max_length=40)
+    plant_id=models.CharField(max_length=3,default='')
+    plant_name=models.CharField(max_length=1000,default='')
+    scientific_name=models.CharField(max_length=1000,default='')
     plant_pic=models.ImageField(upload_to='media/')   #upload_to='/static/media'
-    season=models.CharField(max_length=200)
-    soil=models.CharField(max_length=100)
-    location=models.CharField(max_length=100)
-    cost=models.CharField(max_length=100)
+    season=models.CharField(max_length=200,default='')
+    soil=models.CharField(max_length=100,default='')
+    location=models.CharField(max_length=100,default='')
+    cost=models.CharField(max_length=100,default='')
+    size=models.CharField(max_length=100,default='')
+    sunlight=models.CharField(max_length=100,default='')
+    medicinal_use=models.CharField(max_length=100,default='')
+    plant_type=models.CharField(max_length=100,default='')
+
     def __str__(self):
         return self.plant_name
     

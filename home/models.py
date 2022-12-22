134f60b2cdee20aeb846c37d1330401f5d0ba1ab
Chanmeet <chanmeetsinghsahni@gmail.com>
2022-12-23 00:57:41 +0530

    Final Commit of 23rd december

--- home/models.py
+++ home/models.py
@@ -13,16 +13,16 @@ class Plant(models.Model) :
     plant_id=models.CharField(max_length=3,default='')
     plant_name=models.CharField(max_length=1000,default='')
     scientific_name=models.CharField(max_length=1000,default='')
-    plant_descriptionp=models.CharField(max_length=1000,default='')
+    plant_description=models.CharField(max_length=1000,default='')
     plant_pic=models.ImageField(upload_to='media/')   #upload_to='/static/media'
-    season=models.CharField(max_length=200,default='')
-    soil=models.CharField(max_length=100,default='')
-    location=models.CharField(max_length=100,default='')
-    cost=models.CharField(max_length=100,default='')
-    size=models.CharField(max_length=100,default='')
-    sunlight=models.CharField(max_length=100,default='')
-    medicinal_use=models.CharField(max_length=100,default='')
-    plant_type=models.CharField(max_length=100,default='')
+    season=models.CharField(max_length=1000,default='')
+    soil=models.CharField(max_length=1000,default='')
+    location=models.CharField(max_length=1000,default='')
+    cost=models.CharField(max_length=1000,default='')
+    size=models.CharField(max_length=1000,default='')
+    sunlight=models.CharField(max_length=1000,default='')
+    medicinal_use=models.CharField(max_length=1000,default='')
+    plant_type=models.CharField(max_length=1000,default='')
 
     def __str__(self):
         return self.plant_name
